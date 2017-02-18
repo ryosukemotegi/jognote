6fd57a0eb7634c563a60c001b9659cb532e0fa55
motegiryosuke <ryousukemotegi@gmail.com>
2017-02-18 16:58:26 +0900

    Initial commit

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,10 @@ module Jognote
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.assets  false
+      g.helper  false
+    end
   end
 end
